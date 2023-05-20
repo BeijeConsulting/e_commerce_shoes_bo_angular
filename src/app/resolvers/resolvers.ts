3c99afa8f161e1e24c47fad7fedcf813f9fb0fd1
Francesco Scialdone <francesco.scialdone@beije.it>
2023-05-20 19:21:22 +0200

    Table Bug Fix

--- src/app/resolvers/resolvers.ts
+++ src/app/resolvers/resolvers.ts
@@ -7,6 +7,11 @@ export const getUsersResolverFn = (route: ActivatedRouteSnapshot) => {
   console.log('Resolver Activated');
   const userService = inject(UserService);
 
+  userService.userTableDataState = { page: 1, size: 10 };
+  userService.employeesTableDataState = { page: 1, size: 10 };
+
+  console.log('UserServiceData from resolver: ', userService);
+
   return forkJoin({
     users: userService.getUsers(1, 10, false),
     employees: userService.getUsers(1, 10, true),
